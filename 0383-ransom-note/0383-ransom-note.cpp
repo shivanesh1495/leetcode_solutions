60712a273f336b04956438ed0ae7a63293a2d559
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-04 00:33:16 +0530

    Time: 0 ms (100%), Space: 11.9 MB (29.85%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> hashmap;
+ 
+        for (char ch : magazine) {
+            hashmap[ch]++;
+        }
+ 
+        for (char ch : ransomNote) {
+            if (hashmap[ch] > 0) {
+                hashmap[ch]--;
+            } else {
+                return false;
+            }
+        }
+ 
+        return true;
+    }
+};

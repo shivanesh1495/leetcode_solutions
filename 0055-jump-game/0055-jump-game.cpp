d64d71bce4e11da40327a9a56f21cbb094a2c02c
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-08 12:47:39 +0530

    Time: 0 ms (100%), Space: 52.4 MB (58.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-       int maximum_reach=0;
-       for(int i=0;i<nums.size();i++){
-        if(i>maximum_reach){
-            return false;
+        int mx=0,i=0;
+        while(i<nums.size()){
+            mx=max(mx,i+nums[i]);
+            if(mx>=nums.size()-1){
+                return true;
+            }
+
+            if(i==mx){
+                return false;
+            }
+            i++;
         }
-        maximum_reach=max(maximum_reach,i+nums[i]);
-       }
-       return true;
+
+
+        return false;
+        
     }
+
 };

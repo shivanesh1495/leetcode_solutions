511514d9f2c3f6de3a894484c46a204fb48a458a
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-11 10:48:54 +0530

    Time: 0 ms (100%), Space: 23.1 MB (56.31%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,66 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-/*class Solution {
-public:
-    int maxd(TreeNode* root){
-        if(!root) return 0;
-
-        return 1 + max(maxd(root->left), maxd(root->right));
-    }
-
-    bool isBalanced(TreeNode* root) {
-        if(!root) return true;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            TreeNode* curr = q.front();
-            q.pop();
-
-            int l = 0, r = 0;
-
-            if(curr->left)
-                l = maxd(curr->left);
-
-            if(curr->right)
-                r = maxd(curr->right);
-
-            if(abs(l - r) > 1)
-                return false;
-
-            if(curr->left)
-                q.push(curr->left);
-
-            if(curr->right)
-                q.push(curr->right);
-        }
-
-        return true;
-    }
-};
-*/
-
 class Solution {
 public:
     int height(TreeNode* root){
-        if(!root) return 0;
-
-        int left = height(root->left);
-        if(left == -1) return -1;
-
-        int right = height(root->right);
-        if(right == -1) return -1;
-
-        if(abs(left - right) > 1)
-            return -1;
-
-        return 1 + max(left, right);
+        if(!root){
+            return 0;
+        }
+        return 1+max(height(root->left),height(root->right));
     }
-
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+        if(!root){
+            return true;
+        }   
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* cur=q.front();
+            q.pop();
+            if(cur->left){
+                q.push(cur->left);
+            }
+            if(cur->right){
+                q.push(cur->right);
+            }
+            if( abs( height(cur->left)-height(cur->right) ) > 1 ){
+                return false;
+            }
+        } 
+        return true;    
     }
 };

cb738fc6d1a6188d62a70b7ef043191d830c6384
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-12 18:30:17 +0530

    Time: 3 ms (64.82%), Space: 8.1 MB (17.17%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,27 +1,5 @@
 class Solution {
 public:
-/*
-//Memoization
-
-    vector<int> dp=vector<int>(1001,-1);
-    int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]=fib(n-1)+fib(n-2);
-
-        return dp[n];       
-        
-    }
-};
-
-*/  
-
-//Tabulation
-
     int fib(int n) {
         if(n<=1){
             return n;
@@ -33,6 +11,6 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-
+        
     }
 };

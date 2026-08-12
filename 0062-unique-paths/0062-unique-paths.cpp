c39981e9d5010e40cba17e4f111e22dfd1b9f734
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-12 19:08:28 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,9 +4,9 @@ public:
         vector<vector<int>> dp(m,vector<int>(n,1));
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j] + dp[i][j-1];
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return dp[m-1][n-1];        
+        return dp[m-1][n-1];
     }
 };

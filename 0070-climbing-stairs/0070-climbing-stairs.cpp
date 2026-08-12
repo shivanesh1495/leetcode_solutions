266571628251e344fe8e1d13242dcb79b6caf62b
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-12 18:38:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,15 +3,14 @@ public:
     int climbStairs(int n) {
         if(n<=2){
             return n;
+            
         }
-        int a=1;
-        int b=2;
-        int c;
-        for(int i=3;i<=n;i++){
-           c=a+b;
-           a=b;
-           b=c;
+        vector<int> dp(n);
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return c;
+        return dp[n-1];                
     }
 };

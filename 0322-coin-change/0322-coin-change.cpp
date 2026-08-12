35be493ea5ee1444ffc2be667db9d1c12367412d
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-12 21:05:20 +0530

    Time: 41 ms (45.67%), Space: 48.8 MB (42.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1));
-
-        for (int i = 0; i < coins.size(); i++) {
-            dp[i][0] = 0;
+        vector<vector<int>> dp (coins.size(),vector<int>(amount+1));
+        
+        for(int i=0;i<coins.size();i++){
+            dp[i][0]=0;
         }
 
-        for (int j = 1; j <= amount; j++) {
-            if (j % coins[0] == 0) {
-                dp[0][j] = j / coins[0];
-            } else {
-                dp[0][j] = amount + 1;
+        for(int j=1;j<=amount;j++){
+            if(j%coins[0]==0){
+                dp[0][j]=j/coins[0];
+            }
+            else{
+                dp[0][j]=amount+1;
             }
         }
 
-        for (int i = 1; i < coins.size(); i++) {
-            for (int j = 1; j <= amount; j++) {
-
-                if (j < coins[i]) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-
-                    dp[i][j] = min(dp[i - 1][j],1+ dp[i][j - coins[i]]);
+        for(int i=1;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                if(j<coins[i]){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=min(dp[i-1][j], 1 + dp[i][j-coins[i]]);
                 }
             }
         }
-
-        if(dp[coins.size() - 1][amount]==amount+1){
+        if(dp[coins.size()-1][amount]==amount+1){
             return -1;
         }
-
-        return dp[coins.size() - 1][amount];
+        return dp[coins.size()-1][amount];
     }
 };

164363d314844fa0dae671f3e3b8bbf610f456f4
Shivanesh MC <mcshivanesh777@gmail.com>
2026-08-13 13:45:28 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,16 +5,8 @@ public:
         int ms = INT_MIN;
         int t;
         for (int i = 0; i < nums.size(); i++) {
-            t = cs + nums[i];
-            if (t < nums[i]) {
-                cs = nums[i];
-            } else {
-                cs = t;
-            }
-
-            if (ms < cs) {
-                ms = cs;
-            }
+           cs=max(nums[i],cs+nums[i]);
+           ms=max(cs,ms);
         }
         return ms;
     }

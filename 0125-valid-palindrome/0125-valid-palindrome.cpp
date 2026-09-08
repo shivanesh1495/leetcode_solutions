905fdd7699472e51164b36a04a1379fafd3375ef
Shivanesh MC <mcshivanesh777@gmail.com>
2026-09-08 09:22:04 +0530

    Time: 0 ms (100%), Space: 10.7 MB (7.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,24 +7,10 @@ public:
                 r+=tolower(s[i]);
             }
         }
-        if(r.size()%2==0){
-            string sr=r.substr(0, r.size()/2);
-            string srr=r.substr(r.size()/2);
-            reverse(srr.begin(),srr.end());
-            if(sr==srr){
-                return true;
-            }
+        string f=r;
+        reverse(f.begin(),f.end());
 
-        }
-        else{
-            string sr=r.substr(0, (r.size()/2));
-            string srr=r.substr(r.size()/2+1);
-            reverse(srr.begin(),srr.end());
-            if(sr==srr){
-                return true;
-            }
-
-        }
-        return false;        
+        return f==r;
+        
     }
 };

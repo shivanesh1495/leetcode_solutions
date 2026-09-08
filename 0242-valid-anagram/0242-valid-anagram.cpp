76f91d5a012ba5ce6fb45bacb99b6f11bf2c039d
Shivanesh MC <mcshivanesh777@gmail.com>
2026-09-08 09:06:41 +0530

    Time: 6 ms (21.95%), Space: 10 MB (11.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_multiset<char> an(s.begin(),s.end());
-        if(s.size()!=t.size()){
-            return false;
+        vector<char> s1(s.begin(),s.end());
+        vector<char> t1(t.begin(),t.end());
+
+        sort(s1.begin(),s1.end());
+        sort(t1.begin(),t1.end());
+
+        if(s1==t1){
+            return true;
         }
-        for(char a:t){
-            auto it =an.find(a);
-            if(it!=an.end()){
-                an.erase(it);
-            }
-        }
-        return an.empty();
+
+        return false;
+
         
     }
 };

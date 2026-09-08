2606985365163abeabd716c756b19897a17a3b5f
Shivanesh MC <mcshivanesh777@gmail.com>
2026-09-08 13:51:34 +0530

    Time: 3 ms (31.32%), Space: 11.8 MB (48.58%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> s(n);
+        vector<string> fb;
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0){
-                s[i-1]="FizzBuzz";
+                fb.push_back("FizzBuzz");
             }
             else if(i%3==0){
-                s[i-1]="Fizz";
+                fb.push_back("Fizz");
             }
             else if(i%5==0){
-                s[i-1]="Buzz";
+                fb.push_back("Buzz");
             }
             else{
-                s[i-1]=to_string(i);
+                fb.push_back(to_string(i));
             }
         }
-        return s;
-        
+        return fb;
     }
 };
